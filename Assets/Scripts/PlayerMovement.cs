f363be983f609b7d81ac2ddca0abe1f2cab53c6c
Rohit981 <46937934+Rohit981@users.noreply.github.com>
2020-02-12 19:17:16 +0000

    tile prefabs created and idle animation added for the player. Working on the movement script for the player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    private float MoveVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            MoveVelocity = -speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            MoveVelocity = speed;
+        }
+
+        rb.velocity = new Vector2(MoveVelocity, rb.velocity.y);
+    }
+}
