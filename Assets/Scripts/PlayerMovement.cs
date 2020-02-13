b4b12f48788213c9c9110f055e1de1b26ed5f799
Rohit981 <46937934+Rohit981@users.noreply.github.com>
2020-02-13 13:03:15 +0000

    Jump Animation, Moving Animation added and changed the Movement script for the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,26 +7,65 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody2D rb;
     [SerializeField] private float speed;
+    [SerializeField] private float JumpHeight;
     private float MoveVelocity;
+    private Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            MoveVelocity = -speed;
-        }
+        MovementInput();
+        
+    }
 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            MoveVelocity = speed;
-        }
+    void MovementInput()
+    {
 
-        rb.velocity = new Vector2(MoveVelocity, rb.velocity.y);
+        float Movement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.position = new Vector2(transform.position.x + Movement, transform.position.y);
+             
+
+        FlipCharacter();
+
+        Jump();
+    }
+
+    void FlipCharacter()
+    {
+        Vector3 characterScale = transform.localScale;
+
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 0.81f;
+            anim.SetBool("IsRunning", true);
+        }       
+
+       else if (Input.GetAxis("Horizontal") < 0)
+       {
+           characterScale.x = -0.81f;
+           anim.SetBool("IsRunning", true);
+       }
+
+       else
+       {
+          anim.SetBool("IsRunning", false);
+
+       }
+
+        transform.localScale = characterScale;
+    }
+
+    void Jump()
+    {
+        float JumpSpeed = Input.GetAxis("Jump") * JumpHeight * Time.deltaTime;
+
+        transform.position = new Vector2(transform.position.x, transform.position.y + JumpSpeed);
     }
 }
