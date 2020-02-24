dde58ccc68b88cc4afa93191ec4b9f7b1db33ea8
rp95 <rohitpathak987@gmail.com>
2020-02-24 19:27:48 +0000

    Added network manager script, TCP and UDP scripts still working on UDP client

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class NetworkManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Generic function to serialize a struct into a byte array
+    public static byte[] SerializeStruct<T>(T str)
+    {
+        int size = Marshal.SizeOf(str);                 //Get the size of the struct in bytes
+        byte[] arr = new byte[size];                    //Create an empty byte array to store the data
+
+        System.IntPtr ptr = Marshal.AllocHGlobal(size); //Allocate a block of memory on the heap
+        Marshal.StructureToPtr(str, ptr, true);         //Move the data from the struct to the heap memory
+        Marshal.Copy(ptr, arr, 0, size);                //Copy the data from the heap to our byte array
+
+        Marshal.FreeHGlobal(ptr);                       //Free the allocated memory
+
+        return arr;
+    }
+
+    //Generic function to serialize a struct into a byte array
+    public static void SerializeStruct<T>(T str, ref byte[] arr, int startIndex)
+    {
+        int size = Marshal.SizeOf(str);                 //Get the size of the struct in bytes
+                                                        // byte[] arr = new byte[size];                    //Create an empty byte array to store the data
+
+        System.IntPtr ptr = Marshal.AllocHGlobal(size); //Allocate a block of memory on the heap
+        Marshal.StructureToPtr(str, ptr, true);         //Move the data from the struct to the heap memory
+        Marshal.Copy(ptr, arr, startIndex, size);                //Copy the data from the heap to our byte array
+
+        Marshal.FreeHGlobal(ptr);                      //Free the allocated memory
+    }
+
+    //Generic function to serialize a struct into a byte array
+    public static T DeserializeStruct<T>(byte[] arr, int startIndex = 0) where T : new()
+    {
+        T str = new T();                                //Create an empty struct to store our data
+        int size = Marshal.SizeOf(str);                 //Get the size of the struct in bytes
+
+        System.IntPtr ptr = Marshal.AllocHGlobal(size); //Allocate a block of memory on the heap
+
+        Marshal.Copy(arr, startIndex, ptr, size);       //Copy the data from the byte array to allocated memory, starting at startIndex
+
+        str = (T)Marshal.PtrToStructure(ptr, typeof(T));//Move the data from the heap memory to the struct
+
+        Marshal.FreeHGlobal(ptr);                       //Free the allocated memory
+
+        return str;
+    }
+}
---
+++ Assets/Scripts/UDPClient.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.InteropServices;
+
+
+public class UDPClient : NetworkManager
+{
+    //Client and port variables
+    UdpClient udpClient;
+    Int32 UDP_port;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initializing port value and client instance
+        UDP_port = 5557;
+        udpClient = new UdpClient();
+
+        //Connecting client to the port
+        udpClient.Connect("127.0.0.1", UDP_port);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Send()
+    {
+        Byte[] sendBytes = new Byte[1 + Marshal.SizeOf(msg)];
+    }
+
+    void Recieve()
+    {
+
+    }
+}
